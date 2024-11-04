0296ad341794611e8ff499c1fbcc04ba78b3a75f
Ricardo Ferreira <ricardoferreiran@gmail.com>
2024-11-05 00:06:31 +0100

    calculadora lista

---
+++ main.js
@@ -0,0 +1,26 @@
+const pantalla = document.getElementById('pantalla');
+
+
+function mostrar(valor){
+   pantalla.value += valor; 
+}
+
+function total(){
+    pantalla.value = eval(pantalla.value)
+}
+
+function cambiar(){
+    pantalla.value = eval(pantalla.value)*-1;
+}
+
+function borrar(){
+    pantalla.value = '';
+}
+
+function borrarUno(){
+    pantalla.value = pantalla.value.slice(0,-1)
+}
+
+function porcentaje(){
+    pantalla.value = eval(pantalla.value) * (0.01)
+}
